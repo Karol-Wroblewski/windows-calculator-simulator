1ba733f29bb7c387da33ac8250ff55ef5eca37ff
Karol-Wroblewski <Karol-Wroblewski@users.noreply.github.com>
2019-07-12 00:30:35 +0200

    Updates

---
+++ precache-manifest.3e8fe66f3bc959eac7a8e98a59c36d56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536108cacab14b6a33d9aa9394b5f6d4",
+    "url": "/windows-calculator-simulator/index.html"
+  },
+  {
+    "revision": "7679d57537020a6161b9",
+    "url": "/windows-calculator-simulator/static/css/main.ba837606.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/windows-calculator-simulator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7679d57537020a6161b9",
+    "url": "/windows-calculator-simulator/static/js/main.da7884b5.chunk.js"
+  },
+  {
+    "revision": "d3904426ceb70e07c5a8",
+    "url": "/windows-calculator-simulator/static/js/runtime~main.8598de0d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/windows-calculator-simulator/precache-manifest.3e8fe66f3bc959eac7a8e98a59c36d56.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/windows-calculator-simulator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
