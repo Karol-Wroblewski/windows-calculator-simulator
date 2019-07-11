9c2bce2d2c2cd1d8501a82aad8e10e150ea1da8d
Karol-Wroblewski <Karol-Wroblewski@users.noreply.github.com>
2019-07-11 18:33:33 +0200

    added memory description

--- src/Value.js
+++ src/Value.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Value(props){
 
-  return(<li key={props.id}>{props.value} <p> <button className="menuMemory" onClick={props.delete.bind(this, props.id)}>MC</button> <button className="menuMemory" onClick={props.add.bind(this, props.id)}>M+</button> <button className="menuMemory" onClick={props.subtract.bind(this, props.id)}>M-</button> </p></li>);
+  return(<li key={props.id}>{props.value} <p> <button className="menuMemory" onClick={props.delete.bind(this, props.id)} title="delete position from memory">MC</button> <button className="menuMemory" onClick={props.add.bind(this, props.id) } title="Add to this position in memory">M+</button> <button className="menuMemory" onClick={props.subtract.bind(this, props.id)} title="Subtract from this position in memory">M-</button> </p></li>);
 }
 
 
